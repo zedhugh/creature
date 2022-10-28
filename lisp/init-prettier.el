9b9523f5a137381146b62f77eebe77d0d4c68d1b
Zedhugh Chen <zedhugh@gmail.com>
2022-10-29 04:10:57 +0800

    treat js-json-mode as json-mode in prettier

--- lisp/init-prettier.el
+++ lisp/init-prettier.el
@@ -8,8 +8,18 @@
              (or (not prettier-version) (prettier--in-node-modules-p)))
     (prettier-mode -1)))
 
+(defun creature/prettier-json-parser ()
+  (if (and buffer-file-name
+           (seq-contains
+            '("package.json" "package-lock.json" "composer.json")
+            (file-name-nondirectory buffer-file-name)))
+      '(json-stringify json json5)
+    '(json json5 json-stringify)))
+
 (with-eval-after-load 'prettier
-  (add-hook 'prettier-mode-hook #'creature/prettier-setup))
+  (add-hook 'prettier-mode-hook #'creature/prettier-setup)
+  (add-to-list 'prettier-major-mode-parsers
+               '(js-json-mode . creature/prettier-json-parser)))
 
 (dolist (hook '(css-mode-hook js-mode-hook typescript-mode-hook))
   (add-hook hook #'prettier-mode))
