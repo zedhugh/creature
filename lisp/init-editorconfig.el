e9851dd9bb1e42569f3344cb1557a9dc3a30e128
Zedhugh Chen <zedhugh@gmail.com>
2022-12-06 00:24:23 +0800

    fix incorrect indention of editorconfig when it starting

--- lisp/init-editorconfig.el
+++ lisp/init-editorconfig.el
@@ -4,6 +4,8 @@
 
 (defun creature/editorconfig-setup ()
   (editorconfig-mode 1)
+  ;; 修复通过打开文件启动该插件时打开的文件缩进配置不对
+  (editorconfig-mode-apply)
   (remove-hook 'find-file-hook #'creature/editorconfig-setup)
   (cancel-timer creature/editorconfig-timer)
   (makunbound 'creature/editorconfig-timer))
