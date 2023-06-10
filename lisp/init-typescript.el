6b7f2d9d0e7bddb7ab15a45ff00d8d6564053f49
Zedhugh Chen <zedhugh@gmail.com>
2023-06-10 17:15:48 +0800

    update external tree-sitter config

--- lisp/init-typescript.el
+++ lisp/init-typescript.el
@@ -1,31 +1,32 @@
 ;; -*- coding: utf-8; lexical-binding: t; -*-
 
-(if (bound-and-true-p creature/treesit-available)
-    (progn
-      (if (treesit-ready-p 'typescript)
-          (add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-ts-mode)))
-      (if (treesit-ready-p 'tsx)
-          (add-to-list 'auto-mode-alist '("\\.tsx\\'" . tsx-ts-mode))))
+(when creature/treesit-available
+  (let ((ts-enable (treesit-ready-p 'typescript))
+        (tsx-enable (treesit-ready-p 'tsx)))
+    (if (and ts-enable tsx-enable)
+        (progn
+          (add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-ts-mode))
+          (add-to-list 'auto-mode-alist '("\\.tsx\\'" . tsx-ts-mode)))
 
-  (add-subdirs-to-load-path (expand-file-name "optional-pkg" creature/config-dir))
-  (require 'typescript-mode)
+      (add-subdirs-to-load-path (expand-file-name "optional-pkg" creature/config-dir))
+      (require 'typescript-mode)
 
-  (define-derived-mode typescript-tsx-mode typescript-mode "Tsx"
-    "Major mode for editing TSX file."
-    (require 'tree-sitter)
-    (require 'tree-sitter-hl)
-    (require 'tree-sitter-langs)
-    (require 'tree-sitter-debug)
-    (require 'tree-sitter-query)
+      (define-derived-mode typescript-tsx-mode typescript-mode "Tsx"
+        "Major mode for editing TSX file."
+        (require 'tree-sitter)
+        (require 'tree-sitter-hl)
+        (require 'tree-sitter-langs)
+        (require 'tree-sitter-debug)
+        (require 'tree-sitter-query)
 
-    (require 'tsi-typescript)
-    (tsi-typescript-mode))
+        (require 'tsi-typescript)
+        (tsi-typescript-mode))
 
-  (add-to-list 'auto-mode-alist '("\\.tsx\\'"  . typescript-tsx-mode))
+      (add-to-list 'auto-mode-alist '("\\.tsx\\'"  . typescript-tsx-mode))
 
-  (with-eval-after-load 'tree-sitter-langs
-    (tree-sitter-require 'tsx)
-    (add-to-list 'tree-sitter-major-mode-language-alist '(typescript-tsx-mode . tsx))))
+      (with-eval-after-load 'tree-sitter-langs
+        (tree-sitter-require 'tsx)
+        (add-to-list 'tree-sitter-major-mode-language-alist '(typescript-tsx-mode . tsx))))))
 
 
 (defun creature/treesit-convert-to-template ()
