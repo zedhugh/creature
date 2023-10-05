bb7efdf45f0e8119462febe5abbf1a80ac530329
Zedhugh Chen <zedhugh@gmail.com>
2023-10-05 22:46:22 +0800

    use ace-window when window more than 3

--- lisp/init-ace-window.el
+++ lisp/init-ace-window.el
@@ -5,7 +5,8 @@
 
 (with-eval-after-load 'ace-window
   (require 'ace-window-posframe)
-  (ace-window-posframe-mode 1))
+  (ace-window-posframe-mode 1)
+  (setq aw-dispatch-when-more-than 3))
 
 (lazy-load-global-keys
  '(("C-x o" . ace-window))
