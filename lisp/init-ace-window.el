694e51c208d69d5a1de4f9a58260fc82105df3b8
Zedhugh Chen <zedhugh@gmail.com>
2024-01-08 02:08:11 +0800

    auto toggle ace-window-posframe-mode

--- lisp/init-ace-window.el
+++ lisp/init-ace-window.el
@@ -4,9 +4,18 @@
 
 
 (with-eval-after-load 'ace-window
-  (require 'ace-window-posframe)
-  (ace-window-posframe-mode 1)
   (setq aw-dispatch-when-more-than 3))
+
+(defun creature/auto-toggle-ace-window-posframe-mode (&rest _ignore)
+  "Enable `ace-window-posframe-mode' when Emacs running with GUI, otherwise disable it."
+  (if (display-graphic-p)
+      (progn
+        (require 'ace-window-posframe)
+        (ace-window-posframe-mode 1))
+    (when (fboundp 'ace-window-posframe-mode)
+      (ace-window-posframe-mode -1))))
+
+(advice-add 'ace-window :before #'creature/auto-toggle-ace-window-posframe-mode)
 
 (lazy-load-global-keys
  '(("C-x o" . ace-window))
