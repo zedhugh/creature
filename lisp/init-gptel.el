4d909919757f069d6a52b2db3deba76c94659bb8
Zedhugh Chen <zedhugh@gmail.com>
2026-05-16 14:55:52 +0800

    modify gptel configuration

--- lisp/init-gptel.el
+++ lisp/init-gptel.el
@@ -10,14 +10,32 @@
   (load-file gptel-autoload-file))
 
 (with-eval-after-load 'gptel
-  (setq gptel-model 'deepseek-coder
-        gptel-backend
-        (gptel-make-openai "DeepSeek"
-          :host "api.deepseek.com"
-          :endpoint "/chat/completions"
-          :stream t
-          :key (lambda ()
-                 (gptel-api-key-from-auth-source "api.deepseek.com"))
-          :models '(deepseek-chat deepseek-coder))))
+  (let (deepseek bigmodel bigmodel-code)
+    (setq deepseek
+          (gptel-make-openai "DeepSeek"
+            :host "api.deepseek.com"
+            :endpoint "/chat/completions"
+            :stream t
+            :key (lambda () (gptel-api-key-from-auth-source "api.deepseek.com"))
+            :models '(deepseek-v4-flash deepseek-v4-pro))
+          bigmodel
+          (gptel-make-openai "BigModel"
+            :host "open.bigmodel.cn"
+            :endpoint "/api/paas/v4/chat/completions"
+            :stream t
+            :key (lambda () (gptel-api-key-from-auth-source "open.bigmodel.cn"))
+            :models '(glm-5.1))
+          bigmodel-code
+          (gptel-make-openai "BigModel-Coding"
+            :host "open.bigmodel.cn"
+            :endpoint "/api/coding/paas/v4/chat/completions"
+            :stream t
+            :key (lambda () (gptel-api-key-from-auth-source "open.bigmodel.cn"))
+            :models '(glm-5.1)))
+
+    (setq gptel-default-mode #'org-mode
+          gptel-backend bigmodel-code
+          gptel-model 'gml-5.1
+          )))
 
 (provide 'init-gptel)
