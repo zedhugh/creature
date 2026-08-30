612d75d2a1bfc3426a98fa63fff6796d6481e4cf
Zedhugh Chen <zedhugh@gmail.com>
2026-08-31 00:47:04 +0800

    add submodule "deflate" needed by "plantuml-mode"

--- lisp/init-plantuml.el
+++ lisp/init-plantuml.el
@@ -3,6 +3,7 @@
 (require 'init-package)
 (add-pkg-in-pkg-dir "plantuml-mode")
 (add-pkg-in-pkg-dir "dash")
+(add-pkg-in-pkg-dir "deflate")
 
 (autoload 'plantuml-mode "plantuml-mode" "" t)
 (add-to-list 'auto-mode-alist '("\\.\\(plantuml\\|pum\\|plu\\)\\'" . plantuml-mode))
